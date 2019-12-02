81c8f52f270ca617f9e9988a9dcdd1b93a865e75
周亚楠 <1414403106@qq.com>
2019-12-02 16:08:50 +0800

    增加撤销、还原、删除还原功能

--- ZHFigureDrawingLayer/ZHFigureDrawingLayer/ZHFigureDrawingView.m
+++ ZHFigureDrawingLayer/ZHFigureDrawingLayer/ZHFigureDrawingView.m
@@ -18,7 +18,12 @@
 @property (nonatomic, assign) NSUInteger figureIndex;
 @property (nonatomic, assign) NSUInteger colorIndex;
 @property (nonatomic, strong) UIButton *menuButton;
+@property (nonatomic, strong) UIButton *nextStepButton;
+@property (nonatomic, strong) UIButton *previousStepButton;
+@property (nonatomic, strong) UIButton *rotateButton;
 @property (nonatomic, strong) NSMutableArray *layerArr;
+@property (nonatomic, strong) NSMutableArray *previousLayerArr;
+@property (nonatomic, strong) NSMutableArray *removeLayerArr;
 @end
 
 @implementation ZHFigureDrawingView
@@ -27,10 +32,14 @@
 {
     if (self = [super initWithFrame:frame]) {
         self.userInteractionEnabled = YES;
+        self.previousLayerArr = [NSMutableArray array];
         self.layerArr = [NSMutableArray array];
+        self.removeLayerArr = [NSMutableArray array];
         self.drawingType = ZHFigureDrawingTypeGraffiti;
         self.lineColor = [UIColor blackColor];
-        [self menuButton];
+        [self setButtonCanTouch:0 enable:NO];
+        [self setButtonCanTouch:1 enable:NO];
+        [self setButtonCanTouch:2 enable:NO];
     }
     return self;
 }
@@ -38,9 +47,7 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     self.isFirstTouch = YES;
-    if (self.drawingLayer) {
-        [self.layerArr addObject:self.drawingLayer];
-    }
+    
 }
 
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
@@ -58,6 +65,18 @@
         [self.drawingLayer movePathWithEndPoint:currentPoint];
     }
     self.isFirstTouch = NO;
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event
+{
+    if (self.drawingLayer) {
+        [self.layerArr addObject:self.drawingLayer];
+        [self.previousLayerArr removeAllObjects];
+        [self setButtonCanTouch:0 enable:NO];
+        [self setButtonCanTouch:1 enable:YES];
+        [self.removeLayerArr removeAllObjects];
+        [self setButtonCanTouch:2 enable:NO];
+    }
 }
 
 //菜单
@@ -82,13 +101,83 @@
     [toolView showInView:self];
 }
 
+//前进一步
+- (void)nextStepButtonClick:(UIButton *)sender
+{
+    if (!sender.selected) {
+        return;
+    }
+    if (!self.previousLayerArr.count) {
+        [self setButtonCanTouch:0 enable:NO];
+        return;
+    }
+    ZHFigureDrawingLayer *drawingLayer = [self.previousLayerArr lastObject];
+    [self.layerArr addObject:drawingLayer];
+    [self.layer addSublayer:drawingLayer];
+    [self.previousLayerArr removeObject:drawingLayer];
+    [self setButtonCanTouch:0 enable:self.self.previousLayerArr.count];
+    [self setButtonCanTouch:1 enable:YES];
+}
+
+//撤销一步
+- (void)previousStepButtonClick:(UIButton *)sender
+{
+    if (!sender.selected) {
+        return;
+    }
+    if (!self.layerArr.count) {
+        [self setButtonCanTouch:1 enable:NO];
+        return;
+    }
+    ZHFigureDrawingLayer *drawingLayer = [self.layerArr lastObject];
+    [self.previousLayerArr addObject:drawingLayer];
+    [self.layerArr removeObject:drawingLayer];
+    [drawingLayer removeFromSuperlayer];
+    [self setButtonCanTouch:0 enable:YES];
+    [self setButtonCanTouch:1 enable:self.layerArr.count];
+}
+
+//清除所有
 - (void)removeAllLayerDrawing
 {
-    [self.drawingLayer removeFromSuperlayer];
     for (ZHFigureDrawingLayer *drawingLayer in self.layerArr) {
+        [self.removeLayerArr addObject:drawingLayer];
         [drawingLayer removeFromSuperlayer];
     }
+    [self.previousLayerArr removeAllObjects];
     [self.layerArr removeAllObjects];
+    [self setButtonCanTouch:0 enable:NO];
+    [self setButtonCanTouch:1 enable:NO];
+    [self setButtonCanTouch:2 enable:YES];
+}
+
+//还原所有
+- (void)rotateButtonClick:(UIButton *)sender
+{
+    if (!sender.selected) {
+        return;
+    }
+    for (ZHFigureDrawingLayer *drawingLayer in self.removeLayerArr) {
+        [self.layer addSublayer:drawingLayer];
+    }
+    [self.layerArr addObjectsFromArray:self.removeLayerArr];
+    [self.removeLayerArr removeAllObjects];
+    [self setButtonCanTouch:1 enable:YES];
+    [self setButtonCanTouch:2 enable:NO];
+}
+
+- (void)setButtonCanTouch:(NSInteger)tag enable:(BOOL)enable
+{
+    if (tag == 0) {
+        self.nextStepButton.selected = enable;
+        self.nextStepButton.alpha = enable ? 1 : 0.5;
+    } else if (tag == 1) {
+        self.previousStepButton.selected = enable;
+        self.previousStepButton.alpha = enable ? 1 : 0.5;
+    } else if (tag == 2) {
+        self.rotateButton.selected = enable;
+        self.rotateButton.alpha = enable ? 1 : 0.5;
+    }
 }
 
 - (UIButton *)menuButton
@@ -102,11 +191,67 @@
         [_menuButton addTarget:self action:@selector(menuButtonClick:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:_menuButton];
         [_menuButton mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.right.bottom.equalTo(self).offset(-30);
-            make.width.height.mas_equalTo(40);
+            make.bottom.equalTo(self.previousStepButton.mas_top).offset(-20);
+            make.centerX.width.height.equalTo(self.previousStepButton);
         }];
     }
     return _menuButton;
+}
+
+- (UIButton *)nextStepButton
+{
+    if (!_nextStepButton) {
+        _nextStepButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_nextStepButton setImage:[UIImage imageNamed:@"class_nextstep2"] forState:UIControlStateNormal];
+        _nextStepButton.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.4];
+        _nextStepButton.layer.masksToBounds = YES;
+        _nextStepButton.layer.cornerRadius = 20.f;
+        [_nextStepButton addTarget:self action:@selector(nextStepButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:_nextStepButton];
+        [_nextStepButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.bottom.equalTo(self.menuButton.mas_top).offset(-20);
+            make.centerX.width.height.equalTo(self.previousStepButton);
+        }];
+    }
+    return _nextStepButton;
+}
+
+- (UIButton *)previousStepButton
+{
+    if (!_previousStepButton) {
+        _previousStepButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_previousStepButton setImage:[UIImage imageNamed:@"class_previousstep2"] forState:UIControlStateNormal];
+        _previousStepButton.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.4];
+        _previousStepButton.layer.masksToBounds = YES;
+        _previousStepButton.layer.cornerRadius = 20.f;
+        [_previousStepButton addTarget:self action:@selector(previousStepButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:_previousStepButton];
+        [_previousStepButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.bottom.equalTo(self.rotateButton.mas_top).offset(-20);
+            make.right.equalTo(self).offset(-30);
+            make.width.height.mas_equalTo(40);
+        }];
+    }
+    return _previousStepButton;
+}
+
+- (UIButton *)rotateButton
+{
+    if (!_rotateButton) {
+        _rotateButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_rotateButton setImage:[UIImage imageNamed:@"class_rotate"] forState:UIControlStateNormal];
+        _rotateButton.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.4];
+        _rotateButton.layer.masksToBounds = YES;
+        _rotateButton.layer.cornerRadius = 20.f;
+        [_rotateButton addTarget:self action:@selector(rotateButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:_rotateButton];
+        [_rotateButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.bottom.equalTo(self).offset(-30);
+            make.right.equalTo(self).offset(-30);
+            make.width.height.mas_equalTo(40);
+        }];
+    }
+    return _rotateButton;
 }
 
 /*
